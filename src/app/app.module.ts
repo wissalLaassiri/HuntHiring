a0c5d7617d28ac3e38e33cedee1adac9e5149c09
unknown <ghizlane.merzagui.essa@gmail.com>
2021-04-26 20:44:49 +0200

    first commit Create a LoginComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './Component/intro/intro.component';
+import { LoginComponent } from './Component/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroComponent
+    IntroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
