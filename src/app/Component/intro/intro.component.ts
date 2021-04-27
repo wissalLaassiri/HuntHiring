6b888d5c661fe01f23aca8cfc519edfe034a505d
wissal Laassiri <wissal.laassiri@gmail.com>
2021-04-27 16:14:07 +0000

    responsive navbar

--- src/app/Component/intro/intro.component.ts
+++ src/app/Component/intro/intro.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss']
 })
+
+
 export class IntroComponent implements OnInit {
 
   constructor() { }
