eecbc6687e6a286217501b7739057486d37c15e8
fatim2 <56084604+fatim2@users.noreply.github.com>
2021-04-28 00:06:50 +0000

    registration forms first commit

---
+++ src/app/Component/company-register/company-register.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { MustMatch } from 'src/_helpers/must-match.validator';
+
+@Component({
+  selector: 'app-company-register',
+  templateUrl: './company-register.component.html',
+  styleUrls: ['./company-register.component.css']
+})
+export class CompanyRegisterComponent implements OnInit {
+  registerForm = new FormGroup({ firstName: new FormControl(''),lastName: new FormControl(''),email: new FormControl(''),password: new FormControl(''),confirmPassword: new FormControl('')});
+  submitted = false;
+  constructor(private FormBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registerForm =this.FormBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
+    });
+  }
+
+  
+  get f(){
+    return this.registerForm.controls;
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.registerForm.invalid){
+      return;
+    }
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  }
+  
+
+}
